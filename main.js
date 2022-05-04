56fbbc5789017c3348818f8a9d8615424d70b78b
xasanboy <xasanboyorifjonov2@gmail.com>
2022-05-04 13:11:51 +0500

    time

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ const secund2 = document.querySelector(".box2");
 const secund3 = document.querySelector(".box3");
 // const secund = document.querySelector(".box");
 
+let d=new Date()
+let second=d.getSeconds()
+let minutes = d.getMinutes();
+let hour = d.getHours();
+
+
 function time(T) {
   if (T == 36100) {
     return 0;
   }
-  secund1.style.transform = `rotateZ(${240+T}deg)`;
+  secund1.style.transform = `rotateZ(${(hour*30)+T}deg)`;
   return setTimeout(function () {
     time(T + 6);
   }, 300000);
@@ -18,7 +24,7 @@ function time2(T) {
   if (T == 361000) {
     return 0;
   }
-  secund2.style.transform = `rotateZ(${150+T}deg)`;
+  secund2.style.transform = `rotateZ(${(minutes*6)+T}deg)`;
   return setTimeout(function () {
     time2(T + 6);
   }, 60000);
@@ -29,9 +35,18 @@ function time3(T) {
   if (T == 3610000) {
     return 0;
   }
-  secund3.style.transform = `rotateZ(${T}deg)`;
+  secund3.style.transform = `rotateZ(${(second*6)+T}deg)`;
   return setTimeout(function () {
     time3(T + 6);
   }, 900);
 }
 time3(0);
+
+console.log(second*6);
+console.log(minutes*6);
+console.log(hour*6);
+
+console.log('=======================================');
+console.log(second);
+console.log(minutes);
+console.log(hour);
